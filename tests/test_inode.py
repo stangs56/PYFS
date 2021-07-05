e1ddb9700f44de76578ccf3af58bbc90ec89e6ee
Nicholas Kroft <stangs56@gmail.com>
2021-07-05 18:39:22 -0400

    Added Inode Unit Tests

---
+++ tests/test_inode.py
@@ -0,0 +1,67 @@
+from sys import implementation
+from typing import List
+from pyfs.inode import Inode
+import unittest
+import io
+import logging
+
+from pyfs.pyfs import PYFS
+from pyfs.inode import InodeEntryExists
+from pyfs.inode_entry import InodeEntry
+from pyfs.constants import DEFAULT_BLOCK_SIZE
+
+from tests.test_common import create_dir_if_not_exists, log_with_debug, log_test_case
+
+logging.basicConfig(filename="test_pyfs.log", level=logging.WARNING)
+
+class TestInode(unittest.TestCase):
+    def test_children(self):
+        raise NotImplementedError()
+
+    def test_is_dir(self):
+        raise NotImplementedError()
+
+    def test_contains_data(self):
+        raise NotImplementedError()
+
+    def test_parent_inode_addr(self):
+        raise NotImplementedError()
+
+    def test_next_inode_addr(self):
+        raise NotImplementedError()
+    
+    def test_data_size(self):
+        raise NotImplementedError()
+    
+    def test_full_inode_data(self):
+        raise NotImplementedError()
+
+    def test_data(self):
+        raise NotImplementedError()
+
+    def test_ls(self):
+        raise NotImplementedError()
+
+    def test_find_entry(self):
+        raise NotImplementedError()
+
+    def test_add_inode_entry(self):
+        raise NotImplementedError()
+
+    def test_check_if_exists(self):
+        raise NotImplementedError()
+
+    def test_create_child_inode(self):
+        raise NotImplementedError()
+    
+    def test_make_dir(self):
+        raise NotImplementedError()
+
+    def test_make_file(self):
+        raise NotImplementedError()
+
+    def test_save(self):
+        raise NotImplementedError()
+        
+    def test__repr__(self):
+        raise NotImplementedError()
